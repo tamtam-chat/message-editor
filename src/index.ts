cfa82ba1e93b34015974ce97541cdfb2b78c3c94
Sergey Chikuyonok <serge.che@gmail.com>
2021-04-22 15:58:40 +0300

    Экспорт всех типов токенов

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-export { default as parse, codePointAt, getText, getLength, Token, TokenType, TokenFormat, ParserOptions } from './parser';
+export { default as parse, codePointAt, getText, getLength, ParserOptions } from './parser';
 export { default as Editor, EditorEvent } from './editor';
 export { default as render, RenderOptions } from './render';
 export { setFormat, setLink, textToMd, mdToText, slice, tokenForPos, TextRange } from './formatted-string';
 export { default as split } from './formatted-string/split';
+
+export { Token, TokenType, TokenFormat, TokenCommand, TokenLink, TokenHashTag, TokenMarkdown, TokenMention, TokenText, TokenUserSticker } from './parser/types';
