2e5d6cdbcd67f898b444ba59e8be13ddf0283c81
Sergey Chikuyonok <serge.che@gmail.com>
2021-08-04 18:52:19 +0300

    Обновление зависимостей
    Отдельная сборка для парсера

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,27 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 
 const mode = process.env.NODE_ENV;
 const production = mode === 'production';
 
-export default {
+export default [{
     input: './src/index.ts',
     output: [
         { format: 'es', file: 'dist/editor.es.js' },
         { format: 'cjs', file: 'dist/editor.cjs.js' }
     ],
     plugins: [
-        resolve({ browser: true }),
-        commonjs(),
-        typescript({
-            tsconfigOverride: {
-                compilerOptions: {
-                    target: 'es5',
-                    module: 'ESNext',
-                    declaration: true
-                }
-            }
-        }),
-        production && terser({
-            module: true
-        })
+        typescript(),
+        production && terser({ module: true })
     ],
-};
+}, {
+    input: './src/parser/index.ts',
+    output: {
+        file: 'dist/parser.js',
+        format: 'es'
+    },
+    plugins: [
+        typescript(),
+        production && terser({ module: true })
+    ]
+}];
