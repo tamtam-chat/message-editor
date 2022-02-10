d2fbac5ef9fd929ac65951cfc89cef4678e8984f
Sergey Chikuyonok <serge.che@gmail.com>
2022-02-10 23:47:48 +0300

    Рабочая сборка через Vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,24 @@
+const editorConfig = {
+    entry: './src/index.ts',
+    fileName: 'editor',
+    formats: ['es', 'cjs'],
+};
+
+const parserConfig = {
+    entry: './src/parser/index.ts',
+    formats: ['es'],
+    fileName: () => 'parser.js',
+};
+
 /** @type {import('vite').UserConfig} */
 const config = {
-
+    build: {
+        emptyOutDir: false,
+        outDir: './dist',
+        lib: process.env.BUILD === 'parser'
+            ? parserConfig
+            : editorConfig
+    }
 };
 
 export default config;
