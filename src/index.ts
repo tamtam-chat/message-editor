c6ec866e3fb22ae4f71da5b0b5a79ca35ad12ab1
Sergey Chikuyonok <serge.che@gmail.com>
2022-12-29 17:45:05 +0300

    Экспорт createTree

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ export type { RenderOptions } from './render';
 export { setFormat, setLink, textToMd, mdToText, slice, tokenForPos } from './formatted-string';
 export type { TextRange } from './formatted-string';
 export { default as split } from './formatted-string/split';
+export { createTree, type Tree } from './parser/tree';
 
 export { TokenType, TokenFormat } from './parser/types';
 export type { Token, TokenCommand, TokenLink, TokenHashTag, TokenMarkdown, TokenMention, TokenText, TokenUserSticker } from './parser/types';
